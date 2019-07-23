ff73c216860aae85b8f9b2fa5ed0b57c18c938f0
joemer-solid <52687027+joemer-solid@users.noreply.github.com>
2019-07-23 14:51:56 -0400

    Initial commit

---
+++ FizzBuzzService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace FizzBuzzApp
+{
+    public sealed class FizzBuzzService
+    {
+        public string GetFizzBuzzEvaluation(int input)
+        {
+            return TranslateFizzBuzzMatchResult(EvaluateInput(input), input);
+        }
+
+        private string TranslateFizzBuzzMatchResult(FizzBuzzMatch fizzBuzzMatchResult, int input)
+        {
+            const string FIZZ = "Fizz";
+            const string BUZZ = "Buzz";
+            string result = string.Empty;
+
+            if (fizzBuzzMatchResult == FizzBuzzMatch.DivByThree)
+            {
+                result = FIZZ;
+            }
+            else if (fizzBuzzMatchResult == FizzBuzzMatch.DivByFive)
+            {
+                result = BUZZ;
+            }
+            else if(fizzBuzzMatchResult == FizzBuzzMatch.DivByThreeAndFive)
+            {
+                result = string.Format("{0} {1}", FIZZ, BUZZ);
+            }
+            else if(fizzBuzzMatchResult == FizzBuzzMatch.None)
+            {
+                result = input.ToString();
+            }
+
+            return result;
+        }       
+
+        private FizzBuzzMatch EvaluateInput(int input)
+        {
+            FizzBuzzMatch fizzBuzzMatchResult = FizzBuzzMatch.None;            
+
+            if (input % 3 == 0)
+            {
+                fizzBuzzMatchResult |= FizzBuzzMatch.DivByThree;                
+            }
+
+            if (input % 5 == 0)
+            {
+               
+                fizzBuzzMatchResult |= FizzBuzzMatch.DivByFive;
+            }
+
+            return fizzBuzzMatchResult;
+        }
+    }
+
+    [Flags()]
+    public enum FizzBuzzMatch : byte
+    {
+        None = 0,
+        DivByThree = 1,
+        DivByFive = 2,    
+        DivByThreeAndFive = 3
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Fizz Buzz Evaluation Results*************");
+            DisplayFizzBuzzEvaluationResult();
+        }
+
+
+        private static void DisplayFizzBuzzEvaluationResult()
+        {
+            FizzBuzzService fizzBuzzService = new FizzBuzzService();
+
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(fizzBuzzService.GetFizzBuzzEvaluation(i));
+            }
+        }
+    }
+}
