2a9c878b8799f4a12e1a18fe0bcbe4117028042e
Joseph Merlino <joe_mer@hotmail.com>
2022-01-28 11:42:44 -0500

    Added a solution file and a variation that uses an inline, single method procedural approach to model a Fizz Buzz solution

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace FizzBuzzApp
         static void Main(string[] args)
         {
             Console.WriteLine("**********Fizz Buzz Evaluation Results*************");
+            Console.WriteLine("Fizz Buzz modeled in an abstracted manner more closely aligned with OOP practices...");
             DisplayFizzBuzzEvaluationResult();
+            Console.WriteLine("Fizz Buzz modeled in a single method in an inline-procedural manner");
+            FizzBuzz();
         }
 
 
@@ -18,6 +21,68 @@ namespace FizzBuzzApp
             for(int i = 1; i <= 100; i++)
             {
                 Console.WriteLine(fizzBuzzService.GetFizzBuzzEvaluation(i));
+            }
+        }
+
+        private static void FizzBuzz()
+        {
+            const byte None = 0;
+            const byte DivByThree = 1;
+            const byte DivByFive = 2;
+            const byte DivByThreeAndFive = 3;
+
+            Console.WriteLine("FizzBuzz Evaluation For Numbers Divisible by 3, 5, and 3 and 5......");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(getFizzBuzzEvaluation(i));
+            }
+
+            string getFizzBuzzEvaluation(int fizzBuzzInput)
+                => translateFizzBuzzMatchResult(evaluateInput(fizzBuzzInput), fizzBuzzInput);
+
+            string translateFizzBuzzMatchResult(byte inputEvalResult, int fizzBuzzInput)
+            {
+                const string FIZZ = "Fizz";
+                const string BUZZ = "Buzz";
+                string result = string.Empty;
+
+                if (inputEvalResult == DivByThree)
+                {
+                    result = FIZZ;
+                }
+                else if (inputEvalResult == DivByFive)
+                {
+                    result = BUZZ;
+                }
+                else if (inputEvalResult == DivByThreeAndFive)
+                {
+                    result = $"{FIZZ} {BUZZ}";
+                }
+                else if (inputEvalResult == None)
+                {
+                    result = fizzBuzzInput.ToString();
+                }
+
+                return $"InputValue: {fizzBuzzInput} FizzBuzzResult: {result}";
+            }
+
+            byte evaluateInput(int input)
+            {
+                byte inputEvalResult = 0;
+
+                if (input % 3 == 0)
+                {
+                    inputEvalResult |= DivByThree;
+                }
+
+                if (input % 5 == 0)
+                {
+
+                    inputEvalResult |= DivByFive;
+                }
+
+                return inputEvalResult;
             }
         }
     }
